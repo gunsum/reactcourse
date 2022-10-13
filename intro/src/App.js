30b27f3c7368fc91008f8530b6b193037b02cf4f
Fatma Oymak <bedizma@hotmail.com>
2022-10-13 22:46:25 +0100

    component example

---
+++ intro/src/App.js
@@ -0,0 +1,16 @@
+
+import Navi from './Navi';
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
+
+function App() {
+  return (
+    <div>
+        <Navi/>
+        <CategoryList/>
+        <ProductList/>
+        </div>
+  );
+}
+
+export default App;
